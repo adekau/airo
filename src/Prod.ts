0311b3d57ae207ed1ed064101c380306464e8b37
Alex Dekau <alexander.j.dekau@wmich.edu>
2019-11-07 03:08:55 -0500

    improve readability

--- src/Prod.ts
+++ src/Prod.ts
@@ -7,9 +7,8 @@ export type Prod<T1, T2> = {
 export const times = <TCtor, T1, T2>(
     f: (constructorFn: TCtor) => T1,
     g: (constructorFn: TCtor) => T2
-) => (
-    constructorInput: TCtor
-): Prod<T1, T2> => ({
-    fst: f(constructorInput),
-    snd: g(constructorInput)
-});
+) =>
+    (constructorInput: TCtor): Prod<T1, T2> => ({
+        fst: f(constructorInput),
+        snd: g(constructorInput)
+    });
