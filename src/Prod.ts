723f160c41a1f9acd214d05e77f0a21b9f60b111
Alex Dekau <alexander.j.dekau@wmich.edu>
2019-11-08 00:06:25 -0500

    fst. snd

--- src/Prod.ts
+++ src/Prod.ts
@@ -1,9 +1,11 @@
-// <f*g> : c -> a*b
+import { Func, func } from "./Func";
+
 export type Prod<T1, T2> = {
     fst: T1;
     snd: T2;
 };
 
+// <f*g> : TDomain -> T1*T2
 export const times = <TDomain, T1, T2>(
     f: (input: TDomain) => T1,
     g: (input: TDomain) => T2
@@ -12,3 +14,9 @@ export const times = <TDomain, T1, T2>(
         fst: f(constructorInput),
         snd: g(constructorInput)
     });
+
+// fst: T1*T2 -> T1
+export const fst = <T1, T2>(): Func<Prod<T1, T2>, T1> => func(p => p.fst);
+
+// snd: T1*T2 -> T2
+export const snd = <T1, T2>(): Func<Prod<T1, T2>, T2> => func(p => p.snd);
