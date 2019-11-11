4e10f9d803597090e8bce001b399db53bb4d705a
Alexander J Dekau <alexander.j.dekau@wmich.edu>
2019-11-10 23:54:52 -0500

    format and comment inl, inr

--- src/Sum.ts
+++ src/Sum.ts
@@ -4,10 +4,21 @@ export type Sum<T1, T2> =
     | { kind: 'left', value: T1 }
     | { kind: 'right', value: T2 };
 
-export const inl = <T1, T2>(): Func<T1, Sum<T1, T2>> => func<T1, Sum<T1, T2>>(x => ({ kind: 'left', value: x}));
-export const inr = <T1, T2>(): Func<T2, Sum<T1, T2>> => func<T2, Sum<T1, T2>>(x => ({ kind: 'right', value: x}));
+// f: T1 -> T1+T2
+export const inl = <T1, T2>(): Func<T1, Sum<T1, T2>> =>
+    func<T1, Sum<T1, T2>>(x => ({
+        kind: 'left',
+        value: x
+    }));
 
-//
+// f: T2 -> T1+T2
+export const inr = <T1, T2>(): Func<T2, Sum<T1, T2>> =>
+    func<T2, Sum<T1, T2>>(x => ({
+        kind: 'right',
+        value: x
+    }));
+
+// [f+g]
 export const plus = <TFDomain, TGDomain, TRange>(
     f: (input: TFDomain) => TRange,
     g: (input: TGDomain) => TRange
