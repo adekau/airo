439d038aa14ff0566eeeff7462acaa9d2dea4421
Alex Dekau <alexander.j.dekau@wmich.edu>
2019-11-11 02:52:43 -0500

    absurd, unit, identity

--- src/Category.ts
+++ src/Category.ts
@@ -1,2 +1,18 @@
+import { Func, func } from "./Func";
+
 export type Zero = never;
 export type One = {};
+
+// absurd: Zero -> a
+export const absurd = <TRange>(): Func<Zero, TRange> =>
+    func<Zero, TRange>(_ => {
+        throw new Error('Does not exist.');
+    });
+
+// unit: a -> One
+export const unit = <TDomain>(): Func<TDomain, One> =>
+    func(_ => ({}));
+
+// identity: a -> a
+export const identity = <T>(): Func<T, T> =>
+    func(x => x);
