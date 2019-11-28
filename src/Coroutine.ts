812639c8c0c2d4966008b9157fffd9ca1d32434f
Alexander Dekau <alexanderdekau@Alexanders-MBP.attlocal.net>
2019-11-28 02:13:10 -0500

    continuation, result, value

--- src/Coroutine.ts
+++ src/Coroutine.ts
@@ -1,14 +1,14 @@
 import { Prod } from "./Prod";
 import { Sum, inl, inr } from "./Sum";
 import { Func } from "./Func";
-import { One } from "./Category";
+import { One, identity } from "./Category";
 
 export type CoRef<TState, TErr, T> = {
     get: Coroutine<TState, TErr, T>;
     set: (_: T) => Coroutine<TState, TErr, One>;
 };
 
-export type CoRet<TState, T> = Prod<TState, T>;
+export type CoRet<TState, T> = Prod<T, TState>;
 export type CoCont<TState, TErr, T> = Prod<Coroutine<TState, TErr, T>, TState>;
 export type CoPreRes<TState, TErr, T> = Sum<TErr, CoRes<TState, TErr, T>>;
 export type CoRes<TState, TErr, T> = Sum<CoCont<TState, TErr, T>, CoRet<TState, T>>;
@@ -33,3 +33,15 @@ export function error<TState, TErr, T>(): Func<TErr, CoPreRes<TState, TErr, T>> 
 export function noError<TState, TErr, T>(): Func<CoRes<TState, TErr, T>, CoPreRes<TState, TErr, T>> {
     return inr<TErr, CoRes<TState, TErr, T>>();
 };
+
+export function continuation<TState, TErr, T>(): Func<CoCont<TState, TErr, T>, CoRes<TState, TErr, T>> {
+    return inl<CoCont<TState, TErr, T>, CoRet<TState, T>>();
+};
+
+export function result<TState, TErr, T>(): Func<CoRet<TState, T>, CoRes<TState, TErr, T>> {
+    return inr<CoCont<TState, TErr, T>, CoRet<TState, T>>();
+};
+
+export function value<TState, T>(): Func<Prod<T, TState>, CoRet<TState, T>> {
+    return identity<CoRet<TState, T>>();
+}
