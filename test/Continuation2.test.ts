0c1493169da8caedcecb9f0e3bbdd10727dbe3fe
Alexander J Dekau <alexander.j.dekau@wmich.edu>
2019-12-19 01:52:10 -0500

    ready for merge

--- test/Continuation2.test.ts
+++
@@ -1,8 +0,0 @@
-import { cont } from '../src/Continuation2';
-
-test('cont2', () => {
-    const r = cont((a: number) => a*a)
-        .bind(cont(x => x.toString()))
-        .bind(cont(y => y.length));
-    expect(r.run(15)).toBe(3);
-});
