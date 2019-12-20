b25d5d22cf25141484d49672e48976e550ebdccb
Alexander J Dekau <alexander.j.dekau@wmich.edu>
2019-12-19 23:17:00 -0500

    Document HKTToKind since it's a weird concept

--- src/Monad/HKT.ts
+++ src/Monad/HKT.ts
@@ -3,6 +3,18 @@ export type HKT<TId, V> = {
     readonly _val: V;
 };
 
+/**
+ * Valid HKT Ids are merged into this (https://www.typescriptlang.org/docs/handbook/declaration-merging.html)
+ * from module declarations in monad files.
+ * @example
+ * ```
+ * declare module './HKT' {
+ *     interface HKTToKind<A> {
+ *         SomeMonadName: SomeMonadName<A>
+ *     }
+ * };
+ * ```
+ */
 export interface HKTToKind<A> {}
 export type HKTS = keyof HKTToKind<any>;
 export type Kind<TId extends HKTS, A> = TId extends HKTS ? HKTToKind<A>[TId] : any;
