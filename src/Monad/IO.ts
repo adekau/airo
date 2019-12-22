050cc6e2afe503f9c794367c883a6c428d88ac31
Alexander Dekau <alexanderdekau@Alexanders-MacBook-Pro.local>
2019-12-22 01:06:08 -0500

    io monad

---
+++ src/Monad/IO.ts
@@ -0,0 +1,27 @@
+import { Monad1 } from './Monad';
+
+declare module './HKT' {
+    interface HKTToKind<A> {
+        IO: IO<A>
+    }
+};
+
+export type IO<A> = () => A;
+
+export const HKTId = 'IO';
+export type HKTId = typeof HKTId;
+
+export const of = <A>(a: A): IO<A> => 
+    () => a;
+
+export const IO: Monad1<HKTId> = {
+    HKT: HKTId,
+
+    map: (fa, f) => () => f(fa()),
+
+    of,
+
+    ap: (fab, fa) => () => fab()(fa()),
+
+    bind: (fa, f) => f(fa())
+};
