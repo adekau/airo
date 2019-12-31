e2a58aa97a22500f825eec305802114096e68e3d
Alexander J Dekau <alexander.j.dekau@wmich.edu>
2019-12-30 23:02:13 -0500

    io tests

---
+++ test/Monad/IO.test.ts
@@ -0,0 +1,35 @@
+import { IO, of, map, bind } from '../../src/Monad/IO';
+import { pipe } from '../../src/Monad/Pipeable';
+
+const io = IO.of('Hello world!');
+describe('IO Monad', () => {
+    it('constructs', () => {
+        expect(io()).toBe('Hello world!');
+    });
+
+    it('maps', () => {
+        const newIO = IO.map(io, s => s.length);
+        expect(newIO()).toBe(12);
+    });
+
+    it('applies', () => {
+        const fnIO = IO.of((x: number) => x * 2);
+        expect(IO.ap(fnIO, IO.of(5))()).toBe(10);
+    });
+
+    it('binds/chains', () => {
+        const newIO = IO.bind(io, a => IO.of(a + ':)'));
+        expect(newIO()).toBe('Hello world!:)');
+    });
+
+    it('pipes', () => {
+        const t = pipe(
+            5,
+            of,
+            map(x => x! + 5),
+            map(x => x.toString()),
+            bind(x => of(x + '!'))
+        );
+        expect(t()).toBe('');
+    });
+});
