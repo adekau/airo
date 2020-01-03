073315e6a641ed45040f7d1fdfbe8d70fede6fee
Alexander J Dekau <alexander.j.dekau@wmich.edu>
2020-01-03 01:13:18 -0500

    t1 in 2 func pipe should not be optional

--- src/Monad/Pipeable.ts
+++ src/Monad/Pipeable.ts
@@ -33,7 +33,7 @@ import { HKT, HKTS, HKTS2, HKTSF } from './HKT';
 
 export function pipe<T1>(v: T1): T1;
 export function pipe<T1, T2>(v: T1, fn1: (t1: T1) => T2): T2;
-export function pipe<T1, T2, T3>(v: T1, fn1: (t1?: T1) => T2, fn2: (t2: T2) => T3): T3;
+export function pipe<T1, T2, T3>(v: T1, fn1: (t1: T1) => T2, fn2: (t2: T2) => T3): T3;
 export function pipe<T1, T2, T3, T4>(v: T1, fn1: (t1: T1) => T2, fn2: (t2: T2) => T3, fn3: (t3: T3) => T4): T4;
 export function pipe<T1, T2, T3, T4>(
     v: T1,
